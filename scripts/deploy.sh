13272546ada02c285a29e03d9de07bb57aa82074
oh980225 <oh980225@gmail.com>
2022-03-13 01:48:42 +0900

    feat: version 2

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,7 +8,7 @@ cp $REPOSITORY/build/build/libs/*.jar $REPOSITORY/jar
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep travis | grep java | awk '{print $1}')
+CURRENT_PID=$(pgrep -lf travis | grep java | awk '{print $1}')
 
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
