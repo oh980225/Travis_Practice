a62a87dd77c5a5bba600ebd63086ce2f3db3655f
oh980225 <oh980225@gmail.com>
2022-03-13 01:15:40 +0900

    chore: 설정 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -30,7 +30,7 @@ chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
-nohup java -jar \
+nohup ~/jdk-17.0.2/bin/java -jar \
     -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
     -Dspring.profiles.active=real \
     $JAR_NAME > $REPOSITORY/monit/nohup.out 2>&1 &
